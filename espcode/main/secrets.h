631dcf1346b410ffbc337def027802909d111455
Shivam Gupta <124909397+shivam-sensei@users.noreply.github.com>
2026-04-14 14:08:40 +0530

    Delete secrets

--- espcode/main/secrets.h
+++
@@ -1,8 +0,0 @@
-// secrets.h (do not commit)
-#pragma once
-#define WIFI_SSID "A.T.O.M_Labs"
-#define WIFI_PASSWORD "atom281121"
-#define FIREBASE_API_KEY "AIzaSyAoer09OK5EPK6oFtCjIs8FxfHUVRYppUA"
-#define FIREBASE_DB_URL "https://lab-server-5128b-default-rtdb.asia-southeast1.firebasedatabase.app"
-#define DISCORD_WEBHOOK "https://discord.com/api/webhooks/1369580386606387221/tlfN0ha-OPRweMG0Art3HREDREAEHkToJC5nTvmJjzEuGaZBisp310lZycZKaViJR9Ew"
-
